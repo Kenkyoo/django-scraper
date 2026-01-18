efb89e0cf937d2215133a6181dc31b4bdd3cebd9
Franco <frfantusatti@protonmail.com>
2026-01-18 19:06:04 -0300

    add database

--- scraper/settings.py
+++ scraper/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'scraper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scraper_db',
+        'USER': 'admin',
+        'PASSWORD': '20090086',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

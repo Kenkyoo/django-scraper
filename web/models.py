554fa9fd069b5af0f461b70629e0bb79e5ff94e4
Franco <frfantusatti@protonmail.com>
2026-01-20 01:24:04 -0300

    add models and migrate

--- web/models.py
+++ web/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    url = models.URLField()
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)

662286212b083d4b658f0ac1bbfa31dc83750190
Franco <frfantusatti@protonmail.com>
2026-01-22 04:46:37 -0300

    edit settings.py

--- scraper/settings.py
+++ scraper/settings.py
@@ -79,13 +79,20 @@ WSGI_APPLICATION = "scraper.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default="postgresql://admin:20090086@localhost:5432/scraper_db",
-        conn_max_age=600,
-    )
-}
-
+# Database
+if "DATABASE_URL" in os.environ:
+    DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "scraper_db",
+            "USER": "admin",
+            "PASSWORD": "20090086",
+            "HOST": "localhost",
+            "PORT": "5432",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
